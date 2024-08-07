e90c8dea0a4e61c7e28484dac4875c936148245f
Bireswarroychowdhury29 <117630509+Bireswarroychowdhury29@users.noreply.github.com>
2024-08-07 12:44:24 +0530

    Slider Images Updated 7/8/24 12:45pm

--- app/src/main/java/com/example/cravify/HomeFragment.java
+++ app/src/main/java/com/example/cravify/HomeFragment.java
@@ -1,11 +1,7 @@
 package com.example.cravify;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +15,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,7 +25,6 @@ import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnima
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
